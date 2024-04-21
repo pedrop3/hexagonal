a2ac06ebdeda0449c996d325749124fad33118a1
Pedro Santos <pedro.santos@imedicina.com.br>
2024-04-21 21:15:25 +0100

    Creating usecase and output Port

---
+++ src/main/java/br/com/learn/hexagonal/application/core/usercase/FindCustomerByIdUseCase.java
@@ -0,0 +1,19 @@
+package br.com.learn.hexagonal.application.core.usercase;
+
+import br.com.learn.hexagonal.application.core.domain.Customer;
+import br.com.learn.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
+public class FindCustomerByIdUseCase {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
+    public Customer find(String id) {
+        return findCustomerByIdOutputPort.find(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found."));
+    }
+
+}
