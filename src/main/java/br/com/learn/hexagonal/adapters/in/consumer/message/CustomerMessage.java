b21b398b25be56f940e6b9c57fcc280b3b407855
Pedro Santos <pedro.santos@imedicina.com.br>
2024-04-23 00:26:27 +0100

    Creating config to Kafka

---
+++ src/main/java/br/com/learn/hexagonal/adapters/in/consumer/message/CustomerMessage.java
@@ -0,0 +1,17 @@
+package br.com.learn.hexagonal.adapters.in.consumer.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerMessage {
+
+    private String id;
+    private String name;
+    private String cpf;
+    private Boolean isValidCpf;
+
+}
